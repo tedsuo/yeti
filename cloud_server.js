5eb2120ff55744b881f2d7b931a9eaddbf2827a6
tedsuo <ted@radicaldesigns.org>
2011-09-04 23:38:47 -0700

    cloud server reconnects when dropped, dried up code

--- cloud_server.js
+++ cloud_server.js
@@ -2,27 +2,32 @@
 var dnode = require('dnode');
 var cloud = require('./cloud.js');
 
+var MC_HOST = 'localhost';
+var MC_PORT = 1338;
+var MC_RECONNECT = 3000;
 
 yeti_cloud = cloud.create();
 var mc_client = dnode(yeti_cloud);
 
+console.log('Trying to connect to MC at '+MC_HOST+':'+MC_PORT);
+mc_client.connect({
+    host:MC_HOST, 
+    port: MC_PORT,
+    reconnect: MC_RECONNECT 
+  },
+  function(remote,conn){
+    mc_client.remote = remote;
+    mc_client.remote_conn = conn;
 
+    conn.on('drop', function(){
+      console.log('Connection to MC dropped from '+MC_HOST+':'+MC_PORT);
     });
+    conn.on('refused', function(){
+      console.log('Connection to MC refused at'+MC_HOST+':'+MC_PORT);
     });
+    conn.on('reconnect', function(){
+      console.log('Trying to reconnect to MC at '+MC_HOST+':'+MC_PORT);
+    });
+    console.log('Connected to MC at '+MC_HOST+':'+MC_PORT);
 });
 
-if(process.env.NODE_ENV == 'production'){
-  console.log('Trying to connect to MC on hailstorm.radicaldesigns.org:1338');
-  mc_client.connect(1338, function(remote, conn){
-    mc_client.remote = remote;
-    mc_client.remote_conn = conn;
-    console.log('Connected to MC on hailstorm.radicaldesigns.org:1338');
-  });
-} else {
-  console.log('Trying to connect to MC on localhost:1338');
-  mc_client.connect(1338, function(remote, conn){
-    mc_client.remote = remote;
-    mc_client.remote_conn = conn;
-    console.log('Connected to MC on localhost:1338');
-  });
-}
